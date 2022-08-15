3e51ff1087182139dda528e7538f92f50d44899e
mohaixiao <396367702@qq.com>
2022-08-15 23:04:29 +0800

    轮播图开发使用Swiper

---
+++ src/components/slider/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { SliderContainer } from "./style";
+import { Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+function Slider(props) {
+  const { bannerList } = props;
+
+  return (
+    <div>
+      <SliderContainer>
+        <div className="before"></div>
+        <div className="slider-container">
+          <div className="swiper-wrapper">
+            <Swiper
+              // install Swiper modules
+              modules={[Pagination, Autoplay]}
+              spaceBetween={10}
+              slidesPerView={1}
+              loop
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              pagination={{ el: ".swiper-pagination", clickable: true }}
+            >
+              {bannerList.map((item, index) => (
+                <SwiperSlide key={index}>
+                  <img
+                    src={item.imageUrl}
+                    alt="推荐"
+                    width="100%"
+                    height="100%"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </SliderContainer>
+    </div>
+  );
+}
+export default React.memo(Slider);
