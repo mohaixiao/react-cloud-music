0d887bdfc39a167fc6c1eb17f58bf356e8b9c67c
mohaixiao <396367702@qq.com>
2022-08-25 16:32:46 +0800

    打包

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://localhost:3000/';
+export const baseUrl = 'https://netease-cloud-music-api-lac-ten.vercel.app/';
 
 // axios的实例及拦截器配置
 const axiosInstance = axios.create({
